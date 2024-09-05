73d88514c1b467e0eea7d1dd125602c5faac4db9
evgenator <zenyavolobuev@mail.ru>
2024-09-05 20:57:32 +0700

    ChatBotForTrackingHabit

--- src/bot/handlers/custom_handlers/menu.py
+++ src/bot/handlers/custom_handlers/menu.py
@@ -13,17 +13,9 @@ async def menu(message: Message):
     header: dict[str, str] | None = await get_header(message.from_user.id)
 
     if header:
-        try:
-            await bot.edit_message_text(chat_id=message.chat.id,
-                                        message_id=message.id,
-                                        text='<strong>Меню</strong>',
-                                        reply_markup=button_menu.menu_button())
-
-        except Exception as exc:
-            print(exc)
-            await bot.send_message(chat_id=message.chat.id,
-                                   text='<strong>Меню</strong>',
-                                   reply_markup=button_menu.menu_button())
+        await bot.send_message(chat_id=message.chat.id,
+                               text='<strong>Меню</strong>',
+                               reply_markup=button_menu.menu_button())
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'back_to_menu')
